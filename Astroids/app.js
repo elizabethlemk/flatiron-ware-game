b9e122b5d5769205c46918bac7883039f2208d27
woodylucas <github email address>
2019-03-05 16:01:16 -0500

    added astroids

--- Astroids/app.js
+++ Astroids/app.js
@@ -1,5 +1,11 @@
 
 const FPS = 30;   //frames per second
+const LASER_MAX = 10 // max amount of lasers on screen at once
+const LASER_SPEED = 500 // speed of lasers pixels per second
+const ROIDS_NUMBER = 3;
+const ROIDS_SIZE = 100;
+const ROIDS_SPEED = 50;
+const ROIDS_VERT = 10;
 const FRICTION = 0.7;
 const SHIP_SIZE = 30 // ship height in pixels
 const SHIP_THRUST = 5; // acceleration of ship in pixels per second
@@ -22,24 +28,44 @@ const ship = {
 
   }
 }
-
+let roids = [];
+createAsteroid();
 // setup addEventListners
 document.addEventListener('keydown', handlerKeydown)
 document.addEventListener('keyup', handlerKeyup)
+
+function createAsteroid(){
+  roids = [];
+  let x, y;
+  for (let i = 0; i < ROIDS_NUMBER; i++) {
+    x = Math.floor(Math.random() * canvas.width);
+    y = Math.floor(Math.random() * canvas.height);
+    roids.push(newAsteroid(x, y));
+  };
+}
 
 
 // set up Game Loop
 setInterval(update, 1000 / FPS);
 
+
+// creating roids
+
+
+
 function handlerKeydown(event) {
   switch(event.keyCode) {
-    case 37:
+    case 32: // keycode for spacebar
+    shootLaser();
     ship.rot = TURN_SPEED / 180 * Math.PI / FPS
     break;
-    case 38:
+    case 37: // keycode arrow left
+    ship.rot = TURN_SPEED / 180 * Math.PI / FPS
+    break;
+    case 38: // keycode arrow up
     ship.thrusting = true;
     break;
-    case 39:
+    case 39: // keycode arrow right
     ship.rot = - TURN_SPEED / 180 * Math.PI / FPS
     break;
  }
@@ -47,6 +73,10 @@ function handlerKeydown(event) {
 
 function handlerKeyup(event) {
   switch(event.keyCode) {
+    case 32: // keycode for spacebar
+    ship.canShoot = true;
+    shootLaser();
+    break;
     case 37:
     ship.rot = 0;
     break;
@@ -59,6 +89,18 @@ function handlerKeyup(event) {
  }
 }
 
+function newAsteroid(x, y){
+  const roid = {
+    x: x,
+    y: y,
+    xv: Math.random() * ROIDS_SPEED / FPS * (Math.random() < 0.5 ? 1 : - 1 ),
+    yv: Math.random() * ROIDS_SPEED / FPS * (Math.random() < 0.5 ? 1 : - 1 ),
+    r: ROIDS_SIZE / 2,
+    a: Math.random() * Math.PI / 2, // radians
+    vertices: Math.floor(Math.random() * (ROIDS_VERT + 1) + ROIDS_VERT / 2)
+  }
+  return roid;
+}
 
 function update() {
   // draw space
@@ -114,6 +156,38 @@ function update() {
   ctx.closePath();
   ctx.stroke();
 
+  // draw asteroids
+  ctx.strokeStyle = 'slategrey'
+  let x, y, r, a, vert;
+  ctx.lineWidth = SHIP_SIZE / 20;
+
+  for(let i = 0; i < roids.length; i++){
+
+// asteroids props
+    x = roids[i].x;
+    y = roids[i].y;
+    r = roids[i].r;
+    a = roids[i].a;
+    vert = roids[i].vertices;
+
+    // draw a path
+    ctx.beginPath();
+    ctx.moveTo(
+      x + r * Math.cos(a),
+      y + r * Math.sin(a)
+    );
+
+    // poly
+    for (var j = 0; j < vert; j++) {
+
+    ctx.lineTo(
+      x + r * Math.cos(a + j * Math.PI * 2 / vert),
+      y + r * Math.sin(a + j * Math.PI * 2 / vert)
+    )
+  };
+  ctx.closePath();
+  ctx.stroke();
+}
 
   // rotate ship
 
@@ -136,6 +210,23 @@ function update() {
   } else if (ship.y > canvas.height + ship.r ) {
     ship.y = 0 - ship.r;
   }
+
+
+function shootLaser() {
+  if (ship.canShoot && ship.lasers.length < LASER_MAX) {
+    ship.lasers.push({
+      x: ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
+      y: ship.y - 4/ 3 * ship.r * Math.sin(ship.a),
+      xv: LASER_SPEED * Math.cos(ship.a) / FPS,
+      yv: LASER_SPEED * Math.cos(ship.a) / FPS
+    })
+  }
+  ship.canShoot = false;
+}
+
+
+// drawing lasers
+
 
 
 }
