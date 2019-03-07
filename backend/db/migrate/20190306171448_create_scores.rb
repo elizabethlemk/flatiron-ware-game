9401cdcd4a9367f241dff28ae03384ae799e8acb
Elizabeth Le <elizabethlemk@yahoo.com>
2019-03-07 14:37:05 -0500

    redid backend

--- backend/db/migrate/20190306171448_create_scores.rb
+++
@@ -1,9 +0,0 @@
-class CreateScores < ActiveRecord::Migration[5.2]
-  def change
-    create_table :scores do |t|
-      t.integer :score
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
