9b3ae7267c802cad28c1bfd870900e53c45bc319
Elizabeth Le <elizabethlemk@yahoo.com>
2019-03-08 02:25:40 -0500

    styled

--- front-end/index.js
+++ front-end/index.js
@@ -153,7 +153,6 @@ function createUser(userName) {
         <button id="reset" onclick="window.location.reload();">Play again?</button>
       `
     }, 3000)
-
     postScore(json.id, score)
   })
 }
